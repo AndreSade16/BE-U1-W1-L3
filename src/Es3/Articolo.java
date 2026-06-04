07496e0cb3c3617fb8c1d02df1ab0b9d32fcf59d
Andrea Saderi <andrea.saderi16@gmail.com>
2026-06-04 16:51:28 +0200

    Es 3 without methods

---
+++ src/Es3/Articolo.java
@@ -0,0 +1,10 @@
+package Es3;
+
+public class Articolo {
+    private static int numPezzi;
+    private String codiceArticolo;
+    private String descrizione;
+    private double prezzo;
+    
+
+}
---
+++ src/Es3/Cliente.java
@@ -0,0 +1,11 @@
+package Es3;
+
+import java.util.Date;
+
+public class Cliente {
+    private String codiceCliente;
+    private String nome;
+    private String cognome;
+    private String email;
+    private Date dataIscrizione;
+}
