e0d4a86d6162ba5d4c8934660f4b5fdcdd19ef9a
Andrea Saderi <andrea.saderi16@gmail.com>
2026-06-04 16:58:55 +0200

    Cliente constructor done

--- src/Es3/Cliente.java
+++ src/Es3/Cliente.java
@@ -1,6 +1,7 @@
 package Es3;
 
 import java.util.Date;
+import java.util.Random;
 
 public class Cliente {
     private String codiceCliente;
@@ -8,4 +9,25 @@ public class Cliente {
     private String cognome;
     private String email;
     private Date dataIscrizione;
+
+    public Cliente(String nome, String cognome, String email) {
+        Random random = new Random();
+        this.codiceCliente = "124135134134";
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.dataIscrizione = new Date();
+
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "codiceCliente='" + codiceCliente + '\'' +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", email='" + email + '\'' +
+                ", dataIscrizione=" + dataIscrizione +
+                '}';
+    }
 }
---
+++ src/Es3/Main.java
@@ -0,0 +1,8 @@
+package Es3;
+
+public class Main {
+    static void main(String[] args) {
+        Cliente costumer1 = new Cliente("Andrea", "Saderi", "andrea.saderi123@gmail.com");
+        System.out.println(costumer1);
+    }
+}
